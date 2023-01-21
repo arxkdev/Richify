d3bd7b65954ca2f394f973bcb3c9a877c2f251f2
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-01-21 01:57:47 -0500

    Added main code from studio + example file

---
+++ Richify.lua
@@ -0,0 +1,75 @@
+local Rich = { };
+
+Rich.ElementTags = {
+	["Font"] = "font",
+	["Stroke"] = "stroke",
+	["Bold"] = "b",
+	["Italic"] = "i",
+	["Underline"] = "u",
+	["Strikethrough"] = "s",
+	["Uppercase"] = "uc",
+	["SmallCaps"] = "sc"
+};
+Rich.AttributeTags = {
+	["Color"] = "color",
+	["Size"] = "size",
+	["Face"] = "face",
+	["Family"] = "family",
+	["Weight"] = "weight",
+	["Transparency"] = "transparency",
+	["Joins"] = "joins",
+	["Thickness"] = "thickness"
+};
+
+function Rich:__call(str, options)
+	local Final = str;
+	Final = tostring(Final);
+
+	for ElementName, ElementValue in pairs(options) do
+		local Tag = self.ElementTags[ElementName];
+		if (Tag == nil) then 
+			continue;
+		end;
+
+		local OpeningTag = Tag;
+		
+		if (type(ElementValue) == "table") then
+			if (next(ElementValue) == nil) then 
+				continue;
+			end;
+
+			for AttributeName, AttributeValue in pairs(ElementValue) do
+				local AttributeTag = self.AttributeTags[AttributeName];
+				if (AttributeTag == nil) then 
+					continue;
+				end;
+
+				local AttributeValueString = "";
+				
+				if (typeof(AttributeValue) == "Color3") then
+					AttributeValueString = 'rgb(' .. tostring(math.round(AttributeValue.R * 255)) .. ',' .. tostring(math.round(AttributeValue.G * 255)) .. ',' .. tostring(math.round(AttributeValue.B * 255)) .. ')';
+				else
+					AttributeValueString = tostring(AttributeValue);
+				end;
+
+				OpeningTag = OpeningTag .. " " .. AttributeTag .. '="' .. AttributeValueString .. '"';
+			end;
+		elseif (ElementValue == false) then
+			continue;
+		end;
+
+		Final = "<" .. OpeningTag .. ">" .. Final .. "</".. Tag ..">";
+	end;
+	
+	return tostring(Final);
+end
+
+Rich.LineBreak = '<br />';
+Rich.LessThan = '&lt;';
+Rich.GreaterThan = '&gt;';
+Rich.Quote = '&quot;';
+Rich.Apostrophe = '&apos;';
+Rich.Ampersand = '&amp;';
+
+setmetatable(Rich, {__call = Rich.__call});
+return Rich;
