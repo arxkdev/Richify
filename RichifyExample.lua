1af687f18a22d110151338530015c3059d24a072
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-01-21 01:59:34 -0500

    Update RichifyExample.lua

---
+++ RichifyExample.lua
@@ -0,0 +1,24 @@
+local Richify = require(game:GetService("ReplicatedStorage").Rich);
+
+-- Constants
+local GEMS = "1,000 Gems!";
+local RichifyExampleLabel = script.Parent.TextLabel;
+
+-- Define our options
+local GemOpts = {
+	Stroke = {
+		Thickness = 1,
+		Transparency = 0.25,
+		Color = Color3.fromRGB(0, 162, 255)
+	},
+	
+	Font = {Color = Color3.fromRGB(255, 255, 255)},
+	Bold = true,
+};
+local TextOps = {
+	Font = {Color = Color3.fromRGB(255, 255, 255)},
+}
+
+-- Richify our text
+local Text = "You have " .. Richify(GEMS, GemOpts);
+RichifyExampleLabel.Text = Richify(Text, TextOps);
